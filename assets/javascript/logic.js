6fb059246e072085ab41e52034d977a35e9e8ead
Kevin J Glasow <kevin.glasow@gmail.com>
2018-03-16 16:32:12 -0400

    added links to JQuery, Firebase server, and Bootstrap

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyBsTvIy8q6B5Jc6Dc_fbGY9PYX_vRtz4a0",
+    authDomain: "job-tinder-167a5.firebaseapp.com",
+    databaseURL: "https://job-tinder-167a5.firebaseio.com",
+    projectId: "job-tinder-167a5",
+    storageBucket: "job-tinder-167a5.appspot.com",
+    messagingSenderId: "66660247629"
+  };
+  firebase.initializeApp(config);
+
+  
