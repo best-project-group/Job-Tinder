264c983496d211cfa13db8ce5254cea7e1a01e9f
Kevin J Glasow <kevin.glasow@gmail.com>
2018-03-17 09:47:54 -0400

    gmaps api demo working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,6 @@
+// GOOGLE MAPS API KEY: AIzaSyCysKLNkJpvd4jHgJeSjfKlKfUSS5TvMXg
+
+
 var config = {
     apiKey: "AIzaSyBsTvIy8q6B5Jc6Dc_fbGY9PYX_vRtz4a0",
     authDomain: "job-tinder-167a5.firebaseapp.com",
@@ -8,4 +11,15 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+
+
+<script src="https://maps.googleapis.com/maps/api/js?key=" + apiKey +"&callback=initMap"
+async defer>
