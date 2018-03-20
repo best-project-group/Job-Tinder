a239bd02428fdf215386f58eee88b42a6003ae78
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-03-19 20:20:48 -0400

    building out the call/returns

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,21 +1,23 @@
-    query = $(".textbox").val();
+ 
     
     
     function show_alert2()
 {
-
+    query = queryText;
     var oArgs = {
-      app_key: "wTFBTHnT7b7cPP6n" ,
-      q: query,
-      where: "cleveland",
-      date: "2013061000-2019062000",
-      include: "tags,categories",
-      page_size: 5,
-      sort_order: "popularity",
+        app_key: "wTFBTHnT7b7cPP6n" ,
+        q: query,
+        where: "cleveland",
+        date: "2013061000-2019062000",
+        include: "tags,categories",
+        page_size: 1,
+        sort_order: "popularity",
    };
 
     EVDB.API.call("/events/search", oArgs, function(oData) {
-      console.log(oData);
+        
+        console.log(oData.events.event[0].venue_name);
+
     });
 }
 
@@ -29,7 +31,10 @@ $(document).ready(function() {
     
     $(".test").on("click", function(event) {
         event.preventDefault();
+        queryText = $(".textBox").val();
+        console.log(queryText)
         show_alert2();
+
     
 
     })
