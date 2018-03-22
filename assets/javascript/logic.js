5fbcb4cc92fdc46cddc5555de91c69f436a94f3e
Kevin J Glasow <kevin.glasow@gmail.com>
2018-03-22 19:01:03 -0400

    breaking point to fix white spaces

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -31,13 +31,16 @@ function initMap() {
       };
       lat = position.coords.latitude;
       long = position.coords.longitude;
+      console.log("Lat and Long from google maps API:");
       console.log(position.coords.latitude);
       console.log(position.coords.longitude);
+      console.log("------------------------------------")
       
       infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       infoWindow.open(map);
       map.setCenter(pos);
+      getEvent();
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -57,19 +60,27 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 // Call the eventful API
 
+var radius = 10
 var eventfulKey = "app_key=d4dVMRcZjgzdC4mP";
-var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=44134"
+var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=" + lat + "," + long + "&within=" + radius
+// var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=44134"
+
 getEvent().then(function(foundEvent) {
  
-//   // JQuery to go here!! 
+  // JQuery to go here!! 
 
-  console.log(foundEvent)
+  // console.log(foundEvent)
 })
 
 function getEvent() {  
 return axios.get(eventfulURL)
   .then(function(axiosResponse) {
-    console.log(axiosResponse.data.events.event)
+    // console.log(axiosResponse)
+    console.log("Lat, Long, eventfulURL from within Axios call:")
+    console.log(lat);
+    console.log(long);
+    console.log(eventfulURL)
+    console.log("------------------------------------")
     var eventArray = []
      axiosResponse.data.events.event.forEach(function(singleEvent) { 
       var event= {
