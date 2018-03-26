df691a846e740e7128ad302cd72f473dfd526c00
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-03-26 01:14:51 -0400

    basic form hookup

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,5 @@
+// GOOGLE MAPS API KEY: AIzaSyCysKLNkJpvd4jHgJeSjfKlKfUSS5TvMXg
+
 var config = {
     apiKey: "AIzaSyBsTvIy8q6B5Jc6Dc_fbGY9PYX_vRtz4a0",
     authDomain: "job-tinder-167a5.firebaseapp.com",
@@ -27,12 +29,16 @@ function initMap() {
       };
       lat = position.coords.latitude;
       long = position.coords.longitude;
+      console.log("Lat and Long from google maps API:");
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      console.log("------------------------------------")
       
       infoWindow.setPosition(pos);
       infoWindow.setContent('Location found.');
       infoWindow.open(map);
       map.setCenter(pos);
-       }, function() {
+    }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
@@ -49,22 +55,29 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
-
 // Call the eventful API
+
+
+// var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=44134"
 
 
 
 function getEvent() {
 
 var searchTerm = $("#search-term").val().trim();
-var radius = 10;
+var category = $("#category").val().trim();
+var radius = 10
 var eventfulKey = "app_key=d4dVMRcZjgzdC4mP";
-var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=" + lat + "," + long + "&within=" + radius + "&q=" + searchTerm;
-
+var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=" + lat + "," + long + "&within=" + radius + "&category=" + category + "&q=" + searchTerm;
 return axios.get(eventfulURL)
   .then(function(axiosResponse) {
+    // console.log(axiosResponse)
+    console.log("Lat, Long, eventfulURL from within Axios call:")
+    console.log(lat);
+    console.log(long);
+    console.log(eventfulURL)
+    console.log("------------------------------------")
     var eventArray = []
-    console.log(axiosResponse);
      axiosResponse.data.events.event.forEach(function(singleEvent) { 
       var event= {
         venueAddress: singleEvent.venue_address,
@@ -76,41 +89,28 @@ return axios.get(eventfulURL)
         startTime: singleEvent.start_time,
         stopTime: singleEvent.stop_time,
         eventTitle: singleEvent.title,
-        eventID: singleEvent.id
+
       }
+      
     eventArray.push(event) 
      })
-     console.log(eventArray[0].eventTitle)
-      $("#event").append(eventArray[0].eventTitle);
-      $("#venue").append(eventArray[0].venueName);
-      $("#description").append(eventArray[0].eventDescripition);
-      $("#external-link").attr("href", eventArray[0].eventURL);
+     $("#event").append(eventArray[0].eventTitle);
+     $("#venue").append(eventArray[0].venueName);
+     $("#description").append(eventArray[0].eventDescripition);
+     $("#external-link").attr("href", eventArray[0].eventURL);
 
 
-       function pullDate() {
+      function pullDate() {
 
-           startTime = moment(new Date(eventArray[0].startTime));
-           timeToDoors = moment().to(startTime)
-           formattedStart = moment(startTime).format("LLLL");
+          startTime = moment(new Date(eventArray[0].startTime));
+          timeToDoors = moment().to(startTime)
+          formattedStart = moment(startTime).format("LLLL");
 
-           $("#description").append(formattedStart + "<br>" + timeToDoors)
+          $("#description").append(formattedStart + "<br>" + timeToDoors)
 
-       }
-      pullDate();
-
-      searchId = eventID;
-
-    console.log(eventArray[0].eventURL) 
-     
-    return(eventArray)
-
-//       /* APPENDS OTHER INFORMATION TO CARD AND ATTACHES THE LINK TO THE BUTTON */
-       
-
-
-         
-
-
+      }
+     pullDate();
+  return(eventArray)
   })
 }
 
@@ -133,64 +133,65 @@ function createCard() {
 }
 
 /* RETRIEVES REQUESTED EVENT INFO AND FILLS THE BLANK CARD*/
-// function pullEvent() {
+function pullEvent() {
 
   /* DEFINES THE PARAMETERS TO CHECK AGAINST THE API */
-  // var eventObject = {
-  //     app_key: "wTFBTHnT7b7cPP6n" ,
-  //     q: $("#search-term").val(),
-  //     where: "cleveland",
-  //     date: "2013061000-2019062000",
-  //     include: "tags,categories",
-  //     sort_order: "popularity",
-  // };
+  var eventObject = {
+      app_key: "wTFBTHnT7b7cPP6n" ,
+      q: $("#search-term").val(),
+      where: "cleveland",
+      date: "2013061000-2019062000",
+      include: "tags,categories",
+      sort_order: "popularity",
+  };
 
   /* SEARCHES THE API USING EVENTOBJECT */
-  // EVDB.API.call("/events/search", eventObject, function(objectData) {
+  EVDB.API.call("/events/search", eventObject, function(objectData) {
 
       /* SIMPLIFIES LATER ENTRIES */
-      // event = objectData.events.event;
+      event = objectData.events.event;
 
       /* LOOPS THROUGH THE RETRIEVED EVENT LIST */
         
-      // console.log(event[i]);
+      console.log(event[i]);
 
       /* FORMATS THE UGLY DATE GIVEN BY THE API AND APPENDS TO THE CARD */
-//       function pullDate() {
+      function pullDate() {
 
-//           startTime = moment(new Date(event[i].start_time));
-//           timeToDoors = moment().to(startTime)
-//           formattedStart = moment(startTime).format("LLLL");
+          startTime = moment(new Date(event[i].start_time));
+          timeToDoors = moment().to(startTime)
+          formattedStart = moment(startTime).format("LLLL");
 
-//           $("#description").append(formattedStart + "<br>" + timeToDoors)
+          $("#description").append(formattedStart + "<br>" + timeToDoors)
 
-//       }
+      }
 
-//       /* APPENDS OTHER INFORMATION TO CARD AND ATTACHES THE LINK TO THE BUTTON */
-//       $("#event").append(event[i].title);
-//       $("#venue").append(event[i].venue_name);
-//       $("#description").append(event[i].description);
-//       pullDate();
-//       $("#external-link").attr("href", event[i].url);
+      /* APPENDS OTHER INFORMATION TO CARD AND ATTACHES THE LINK TO THE BUTTON */
+      $("#event").append(event[i].title);
+      $("#venue").append(event[i].venue_name);
+      $("#description").append(event[i].description);
+      pullDate();
+      $("#external-link").attr("href", event[i].url);
 
-//       searchId = eventObject.q;
-//   });
-// }
+      searchId = eventObject.q;
+  });
+}
 
 
-
+$(document).ready(function() {
 
   searchId = "";
 
   i = 0;
-$("#submit-btn").on("click", function(event) {
+$("#submit-btn").on("click", function() {
   event.preventDefault();
     $('html, body').animate({
         scrollTop: $("#top-of-form").offset().top
     }, 5000);
-    
+     event.preventDefault();
       createCard();
-      getEvent();
+      pullEvent();
+      console.log(lat, long);
 
       /* IF THE SEARCH TERM IS NEW, START CYCLE AT 0, ELSE ITERATE */
       if($("#search-term").val() === searchId) {
@@ -201,7 +202,6 @@ $("#submit-btn").on("click", function(event) {
       }
 
   var zipCode = $("#zip-code").val().trim();
-  console.log("zip code: " +zipCode);
 
   var today =new Date();
   var inputDate1 = new Date($('#date1').val());
@@ -217,4 +217,6 @@ $("#submit-btn").on("click", function(event) {
     $("#date").append(dateMessg);
   }
 });
+});
+
 
