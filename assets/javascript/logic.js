bb1e0dbae2267c87ddc9d3b0ff321193404645cf
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-03-26 04:56:39 -0400

    cycling working and category/date tied. might remove cycling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -66,9 +66,10 @@ function getEvent() {
 
 var searchTerm = $("#search-term").val().trim();
 var category = $("#category").val().trim();
-var radius = 10
+var date = "future";
+var radius = 10;
 var eventfulKey = "app_key=d4dVMRcZjgzdC4mP";
-var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=" + lat + "," + long + "&within=" + radius + "&category=" + category + "&q=" + searchTerm;
+var eventfulURL = "https://api.eventful.com/json/events/search?" + eventfulKey + "&location=" + lat + "," + long + "&within=" + radius + "&c=" + category + "&q=" + searchTerm + "&t=" + date;
 return axios.get(eventfulURL)
   .then(function(axiosResponse) {
     // console.log(axiosResponse)
@@ -91,13 +92,16 @@ return axios.get(eventfulURL)
         eventTitle: singleEvent.title,
 
       }
-      
+    
     eventArray.push(event) 
      })
-     $("#event").append(eventArray[0].eventTitle);
-     $("#venue").append(eventArray[0].venueName);
-     $("#description").append(eventArray[0].eventDescripition);
-     $("#external-link").attr("href", eventArray[0].eventURL);
+     
+     searchID = $("#search-term").val().trim();
+     categoryID = $("#category").val().trim();
+     $("#event").append(eventArray[i].eventTitle);
+     $("#venue").append(eventArray[i].venueName);
+     $("#description").append(eventArray[i].eventDescripition);
+     $("#external-link").attr("href", eventArray[i].eventURL);
 
 
       function pullDate() {
@@ -132,74 +136,34 @@ function createCard() {
 
 }
 
-/* RETRIEVES REQUESTED EVENT INFO AND FILLS THE BLANK CARD*/
-function pullEvent() {
-
-  /* DEFINES THE PARAMETERS TO CHECK AGAINST THE API */
-  var eventObject = {
-      app_key: "wTFBTHnT7b7cPP6n" ,
-      q: $("#search-term").val(),
-      where: "cleveland",
-      date: "2013061000-2019062000",
-      include: "tags,categories",
-      sort_order: "popularity",
-  };
-
-  /* SEARCHES THE API USING EVENTOBJECT */
-  EVDB.API.call("/events/search", eventObject, function(objectData) {
-
-      /* SIMPLIFIES LATER ENTRIES */
-      event = objectData.events.event;
-
-      /* LOOPS THROUGH THE RETRIEVED EVENT LIST */
-        
-      console.log(event[i]);
-
-      /* FORMATS THE UGLY DATE GIVEN BY THE API AND APPENDS TO THE CARD */
-      function pullDate() {
-
-          startTime = moment(new Date(event[i].start_time));
-          timeToDoors = moment().to(startTime)
-          formattedStart = moment(startTime).format("LLLL");
-
-          $("#description").append(formattedStart + "<br>" + timeToDoors)
-
-      }
-
-      /* APPENDS OTHER INFORMATION TO CARD AND ATTACHES THE LINK TO THE BUTTON */
-      $("#event").append(event[i].title);
-      $("#venue").append(event[i].venue_name);
-      $("#description").append(event[i].description);
-      pullDate();
-      $("#external-link").attr("href", event[i].url);
-
-      searchId = eventObject.q;
-  });
-}
 
 
-$(document).ready(function() {
 
-  searchId = "";
+var i = 0;
 
-  i = 0;
-$("#submit-btn").on("click", function() {
+var searchID = "";
+var categoryID = "";
+
+
+
+$("#submit-btn").on("click", function(event) {
   event.preventDefault();
     $('html, body').animate({
         scrollTop: $("#top-of-form").offset().top
     }, 5000);
-     event.preventDefault();
-      createCard();
-      pullEvent();
-      console.log(lat, long);
 
-      /* IF THE SEARCH TERM IS NEW, START CYCLE AT 0, ELSE ITERATE */
-      if($("#search-term").val() === searchId) {
-        i++;
-      }
-      else {
-        i = 0;
-      }
+    /* IF THE SEARCH TERM IS NEW, START CYCLE AT 0, ELSE ITERATE */
+    if($("#search-term").val().trim() === searchID && $("#category").val().trim() === categoryID) {
+      i++;
+    }
+    else {
+      i = 0;
+    }
+    
+
+    createCard();
+    getEvent();
+
 
   var zipCode = $("#zip-code").val().trim();
 
@@ -217,6 +181,6 @@ $("#submit-btn").on("click", function() {
     $("#date").append(dateMessg);
   }
 });
-});
+
 
 
